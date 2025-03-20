1dc1d798c55c6025e8d618fd2bbef1f52b922f93
Kaian Freitas <kaian.freitas@hotmail.com>
2025-03-20 10:33:47 -0300

    Primeiro commit

---
+++ src/main/java/com/assistente_Rotina/assistente_Rotina/AssistenteRotinaApplication.java
@@ -0,0 +1,13 @@
+package com.assistente_Rotina.assistente_Rotina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistenteRotinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistenteRotinaApplication.class, args);
+		System.out.println("Assistente de rotina iniciado...");
+	}
+}
---
+++ src/test/java/com/assistente_Rotina/assistente_Rotina/AssistenteRotinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assistente_Rotina.assistente_Rotina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistenteRotinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
