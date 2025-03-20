ec8b05d9076b4345012cfb3093e17a5e3f392762
Kaian Freitas <kaian.freitas@hotmail.com>
2025-03-20 19:08:09 -0300

    Att 03

--- src/main/java/com/assistente_Rotina/assistente_Rotina/controller/GoogleCalendarController.java
+++ src/main/java/com/assistente_Rotina/assistente_Rotina/controller/GoogleCalendarController.java
@@ -1,6 +1,24 @@
 package com.assistente_Rotina.assistente_Rotina.controller;
 
+import com.assistente_Rotina.assistente_Rotina.service.GoogleCalendarService;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
+//Controlador REST para acessar os eventos
+@RestController
+@RequestMapping("/api/calendar")
 public class GoogleCalendarController {
+
+    @GetMapping("/eventos")
+    public String getEventosDoDia() {
+        try {
+            GoogleCalendarService.listarEventosDoDia();
+            return "Eventos carregados.";
+        } catch (IOException | GeneralSecurityException e) {
+            return "Erro ao buscar eventos: " + e.getMessage();
+        }
+    }
 }
+
